6f800c14262784708fe06d8c0f95b71a1bcdceb7
Jack Greenaway <jack_greenaway@outlook.com>
2023-03-11 15:34:49 +0000

    Add project files.

---
+++ ATM_Application/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace atm_program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the bank of Jack");
+            Console.WriteLine("---------------------------\n");
+
+            // initiate pin_code variable
+            string pin_code = "";
+            Console.WriteLine("Please enter your PIN code:");
+            pin_code = Console.ReadLine();
+
+        // check the validity of the PIN code
+            // length of PIN code
+            // too long
+            while (pin_code.Length > 4)
+            {
+                Console.WriteLine($"You have entered a PIN code too long ({pin_code.Length}), please re-enter:");
+                pin_code = Console.ReadLine();
+            }
+            // too short
+            while (pin_code.Length < 4)
+            {
+                Console.WriteLine($"You have entered a PIN code too short ({pin_code.Length}), please re-enter:");
+                pin_code = Console.ReadLine();
+            }
+            // numbers only
+            double clean_pin_code = 0;
+            while (!double.TryParse(pin_code, out clean_pin_code))
+            {
+                Console.WriteLine("You have used letters");
+                Console.WriteLine("Please enter a 4 digit PIN code:");
+                pin_code = Console.ReadLine();
+
+            }
+        }
+    }
+}
+
